dd7a41e8d9d1cd92f852f4e07dcf0ac0b4ccd6d3
Nick Romano <nick.r.romano@gmail.com>
2022-06-14 21:09:10 -0700

    Upgrade gzip

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "WatchSync", targets: ["WatchSync"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/1024jp/GzipSwift", from: "4.1.0"),
+        .package(url: "https://github.com/1024jp/GzipSwift", from: "5.0.0"),
     ],
     targets: [
         .target(
